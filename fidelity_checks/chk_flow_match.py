06f41622fe3339cad56f0a824c4712b0865a0ae6
ezrakahn <ezra.kahn@ars.usda.gov>
2016-12-07 05:15:57 -0500

    added dropped flows check

--- fidelity_checks/chk_flow_match.py
+++ fidelity_checks/chk_flow_match.py
@@ -15,10 +15,18 @@ from funcs import *
 
 '''
 To Do:
-1) Want to use a dialog link Tkinter widget to get a text to label the tables for output.
+1) Test with full data table dump. Currently using just subset.
+
+2) Need better streamline of workflow.  It currently asks you to select files too many times and it's annoying.
+	Also, need to get the file writer a little smarter about the size of the list it's receiving.
+
+3) Need to finish the report document writer.
+
+Later:
+--	Want to use a dialog link Tkinter widget to get a text to label the tables for output.
 	Also, is there a way to message the user with the file dialog?
 	https://docs.python.org/3.4/library/tkinter.html#tkinter-modules
-3) Figure out the error handling so we can get better messages
+
 '''
 
 
@@ -40,9 +48,10 @@ def checkTwoDatabases():
 	#With the id_match, find any mismatched names.
 	name_mismatch = id_match[~id_match['name'].isin(right['name'])]
 
-	#diff = right_in_left[~right_in_left.isin(left_in_right)]
 	print('matching ids: \n', id_match.to_string(), '\n \n')
 	print('mismatched names (left names not in right flow name list): \n', name_mismatch.to_string())
+
+	return [id_match, name_mismatch]
 
 
 def checkThreeDatabases():
@@ -50,5 +59,60 @@ def checkThreeDatabases():
 
 	#First, get the separate database flow files
 	files = getTwoFilenames()
+	filenames = files[0]
+	filetypes = files[1]
 
-#checkTwoDatabases()
+	#Write these to pandas datafames
+	dfone = read_file(filenames[0], filetypes[0])
+	dftwo = read_file(filenames[1], filetypes[1])
+
+	#Get the merged dataset
+	file = getOneFilename()
+	filename = file[0]
+	filetype = file[1]
+
+	merged = read_file(filename,filetype)
+
+	#Check if the any of the flows are not in the merged dataframe
+	dropped_one = dfone[~dfone['ref_id'].isin(merged['ref_id'])]
+	dropped_two = dftwo[~dftwo['ref_id'].isin(merged['ref_id'])]
+
+	print('dropped flows from first dataset: \n', dropped_one.to_string(), '\n \n')
+	print('dropped flows from second dataset: \n', dropped_two.to_string(), '\n \n')
+
+	return [dropped_one, dropped_two]
+
+def writereport(analysis):
+	#This doesn't quite work yet.  The report string is not the right form.
+	#Generate the report
+	header1 = 'matching ids: \n'
+	header2 = 'mismatched names (left names not in right flow name list): \n'
+	header3 = 'dropped flows from first dataset: \n'
+	header4 = 'dropped flows from second dataset: \n'
+
+	report = [header1, analysis[0][0], header2, analysis[0][1], header3, analysis[1][0], header4, analysis[1][1]]
+
+	#return print(report)
+	#Get down to the business of writing the file
+	filename = fd.asksaveasfilename()
+
+	with open(filename, 'w') as report_file:
+		'''
+		report_file.write('matching ids: \n',
+						  analysis[0][0],
+						  '\n \n mismatched names (left names not in right flow name list):',
+						  analysis[0][1],
+						  '\n \n dropped flows from first dataset:',
+						  analysis[1][0],
+						  'dropped flows from second dataset:',
+						  analysis[1][1]
+		)
+		'''
+		output =
+		report_file.write('\n'.join(report))
+
+analysis=[]
+analysis.append(checkTwoDatabases())
+analysis.append(checkThreeDatabases())
+
+writereport(analysis)
--- fidelity_checks/funcs.py
+++ fidelity_checks/funcs.py
@@ -1,13 +1,15 @@
 __author__ = 'ekahn'
-__project__ = 'fidelity_checkis'
+__project__ = 'fidelity_checks'
 '''
-Suuporting functionality for the LCA openLCA data curation business.
+Supporting functionality for the LCA openLCA data curation business.
 '''
 import pandas as pd
 from tkinter import filedialog as fd
 import re
+from sys import exit
 
-#We want two files, not less, not more
+'''
+#Not using this function anymore
 def check_file_size(files):
 	x = len(files)
 	if x > 2:
@@ -17,24 +19,32 @@ def check_file_size(files):
 		print('Select two data files for comparison')
 		exit
 	return
+'''
 
-#This is the read file business.
-def read_file(file,filetype):
+
+def read_file(filename, filetype):
+	#This is the read file business.
 	if filetype == 'csv':
-		df = pd.read_csv(file)
+		df = pd.read_csv(filename)
 	elif filetype == 'xls':
-		df = pd.read_excel(file)
+		df = pd.read_excel(filename)
 	elif filetype == 'xlsx':
-		df = pd.read_excel(file)
+		df = pd.read_excel(filename)
 	else:
-		return print('file needs to be either excel or csv')
-		exit	#This get's us out of this function without returning anything, but the script carries on.
+		exit('error regarding filetype.  files can be either xls, xlsx or csv only, and should be olca txt dumps')
 
 	return df.dropna()
 
+
 def getTwoFilenames():
-	files = fd.askopenfilenames()
-	check_file_size(files)
+	files = fd.askopenfilenames(title='Select two files containing flows to compare')
+
+	#check_file_size(files)
+	#We want two files, not less, not more
+	if len(files) > 2:
+		exit("You selected too many files, select 2 only")
+	elif len(files) < 2:
+		exit("Select two data files for comparison, you didn't select enough")
 
 	file1 = files[0]
 	file2 = files[1]
@@ -43,10 +53,22 @@ def getTwoFilenames():
 
 	#Get the file type extensions
 	p = re.compile(r'\.')
-	file1_type = re.split(p,file1)[-1]
-	file2_type = re.split(p,file2)[-1]
+	file1_type = re.split(p, file1)[-1]
+	file2_type = re.split(p, file2)[-1]
 
 	filetypes = [file1_type, file2_type]
 
 	return [files, filetypes]
 
+
+def getOneFilename():
+	file = fd.askopenfilename(title='Select a file containing merged flows')
+
+	#Get the file type extensions
+	p = re.compile(r'\.')
+	filetype = re.split(p, file)[-1]
+
+	return [file, filetype]
+
+#getOneFilename()
+#getTwoFilenames()
